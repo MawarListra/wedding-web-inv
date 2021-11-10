6666f925e96b4130993fb934ba1bc9cea7fbc37c
lukibp97 <45198029+lukibp97@users.noreply.github.com>
2021-11-10 22:49:16 +0700

    karu ucapan fix

--- src/Pages/Admin/Kartu.js
+++ src/Pages/Admin/Kartu.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
-import React,{ useEffect } from 'react'
-import Card1 from '../../Images/WishesCard 1.png'
-import Card2 from '../../Images/WishesCard 2.png'
-import Card3 from '../../Images/WishesCard 3.png'
-import Card4 from '../../Images/WishesCard 4.png'
+import React, { useEffect } from 'react'
+import Card1 from '../../Images/L1.png'
+import Card2 from '../../Images/L2.png'
+import Card3 from '../../Images/L3.png'
+import Card4 from '../../Images/L4.png'
 import { useParams } from "react-router-dom";
+import Loader from "react-loader-spinner";
 
 function Kartu() {
 
@@ -18,7 +19,7 @@ function Kartu() {
     useEffect(() => {
         getUserById();
         //console.log(id);
-    },);
+    });
     const getUserById = async () => {
 
         const config = {
@@ -30,12 +31,12 @@ function Kartu() {
         await axios.get(`${url}/v1/Undangan/${id}`, config)
             .then(response => {
                 const datas = response.data.data;
-                datas.map(tamu =>(
-                   ( setNama(tamu.name),
-                    setMessage(tamu.message),
-                    setGambar(tamu.tempelate))
+                datas.map(tamu => (
+                    (setNama(tamu.name),
+                        setMessage(tamu.message),
+                        setGambar(tamu.tempelate))
                 ))
-              
+
 
                 console.log(response.data.data);
             }).catch((err) => {
@@ -47,17 +48,22 @@ function Kartu() {
         <div>
             <div className="flex justify-center items-center fixed inset-0">
                 <div className="flex relative">
-                    <img src={gambar===1?Card1:gambar===2?Card2:gambar===3?Card3:Card4} alt={gambar===1?'card 1':gambar===2?'card 2':gambar===3?'card 3':'card 4'} className="transform scale-150"></img>
-                    <div className="flex justify-center fixed inset-x-0 mt-7">
+                    {gambar === '' || gambar === null ? <Loader
+                        type="ThreeDots"
+                        color="#00BFFF"
+                        height={100}
+                        width={100}
+                    /> : <img src={gambar === 1 ? Card1 : gambar === 2 ? Card2 : gambar === 3 ? Card3 : Card4} alt={gambar === 1 ? 'card 1' : gambar === 2 ? 'card 2' : gambar === 3 ? 'card 3' : 'card 4'} className="object-fill m-auto w-full max-w-md border-2 rounded-xl"></img>}
+                    <div className="flex justify-center fixed inset-x-0 mt-16">
                         <div className="font-sans text-3xl font-bold"> {nama} </div>
                     </div>
                     <div className="flex justify-center fixed inset-1/3 mt-7">
                         <div className="w-96 text-center break-all font-sans text-sm"> {message} </div>
                     </div>
                 </div>
-               
+
             </div>
-            
+
         </div>
     )
 }
